bc16a484577ba8a1890961453e0afc10ecd4e56e
efunn <e@ethanoblak.com>
2017-12-28 19:46:34 -0800

    testing manual build

--- _plugins/jekyll-scholar-extras.rb
+++ _plugins/jekyll-scholar-extras.rb
@@ -1,8 +1,11 @@
-#require 'jekyll/scholar'
+# require 'jekyll/scholar'
 # require 'jekyll-scholar-extras'
-require 'rubygems'
-require 'bundler/setup'
-Bundler.setup(:default, :ci)
+################################################
+# existence of gemfile messes up jekyll build? #
+################################################
+# require 'rubygems'
+# require 'bundler/setup'
+# Bundler.setup(:default, :ci)
 require 'jekyll/scholar/extras'
 # require '/Library/Ruby/Gems/2.0.0/gems/jekyll-scholar-extras-0.1.3/lib/jekyll/scholar/extras.rb'
 # require_all '/Library/Ruby/Gems/2.0.0/gems/jekyll-scholar-extras-0.1.3/'
