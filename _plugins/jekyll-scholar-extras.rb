9db2a8e515367ad3d8b6675139d67475c8d778c7
efunn <e@ethanoblak.com>
2017-12-28 17:41:57 -0800

    bundler setup

--- _plugins/jekyll-scholar-extras.rb
+++ _plugins/jekyll-scholar-extras.rb
@@ -1,5 +1,8 @@
 #require 'jekyll/scholar'
 # require 'jekyll-scholar-extras'
+require 'rubygems'
+require 'bundler/setup'
+Bundler.setup(:default, :ci)
 require 'jekyll/scholar/extras'
 # require '/Library/Ruby/Gems/2.0.0/gems/jekyll-scholar-extras-0.1.3/lib/jekyll/scholar/extras.rb'
 # require_all '/Library/Ruby/Gems/2.0.0/gems/jekyll-scholar-extras-0.1.3/'
